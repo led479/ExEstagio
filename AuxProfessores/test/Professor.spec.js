ca623ac383e84783c21d6e657ae43a808fc3e402
Rolf Zambon <rolf.zambon@grad.ufsc.br>
2019-05-22 17:46:06 -0300

    Finaliza teste do componente Professor.vue

--- AuxProfessores/test/Professor.spec.js
+++ AuxProfessores/test/Professor.spec.js
@@ -4,27 +4,30 @@ import Professor from '@/components/Professor.vue'
 import { createLocalVue } from '@vue/test-utils'
 import BootstrapVue from 'bootstrap-vue/es'
 
-// create an extended `Vue` constructor
 const localVue = createLocalVue()
-
-// install plugins as normal
 localVue.use(BootstrapVue)
 
+const professor = {
+  nome: "Professor de Teste",
+  login: "teste123"
+}
 
+const wrapper = mount(Professor, {
+  localVue,
+  propsData: {
+    professor: professor
+  }
+})
 
 describe('Professor.vue', () => {
-  test('emite evento abrirAvisos ao clicar no link criar aviso', () => {
-    const wrapper = mount(Professor, {
-      localVue,
-      propsData: {
-        professor: {
-          nome: "Professor de Teste",
-          login: "teste123"
-        }
-      }
-    })
+  test('possui o nome e login do professor, e um link com o texto Criar aviso', () => {
+    expect(wrapper.text()).toContain(professor.nome)
+    expect(wrapper.text()).toContain(professor.login)
     expect(wrapper.text()).toContain('Criar aviso')
-    wrapper.find('a#link-aviso-teste123').trigger('click')
-    expect(wrapper.emitted('abrirAvisos')).toBeTruthy()
+  })
+
+  test('emite o evento abrirAvisos passando o professor ao clicar em Criar aviso', () => {
+    wrapper.find(`a#link-aviso-${professor.login}`).trigger('click')
+    expect(wrapper.emitted().abrirAvisos[0]).toEqual([professor])
   })
 })
